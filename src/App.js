1cea634148d82efadef33025c04976eda4aa9256
cristian <cristian-mansilla@live.com.ar>
2025-06-04 11:53:07 -0300

    primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 
 const App = () => {
-  
+
   const nombreRef = useRef();
   const resultadoRef = useRef();
 
@@ -16,5 +16,28 @@ const App = () => {
   </div>
 
 };
+
+// const App = () => {
+//   const titulo = <h1>Aprendiendo React</h1>;
+//   const btnAlert = () => {
+//     alert("Saluda React");
+//   };
+//   const array = [1, 2, 3, 4, 5];
+//   return (
+//     <>
+//       <div>
+//         {titulo}
+//       </div>
+//       <div>
+//         {array.map((elem,key)=>{
+//           return <p key={key}>{elem}</p>
+//         })}
+//       </div>
+//       <div>
+//         <button onClick={btnAlert}>Presioname</button>
+//       </div>
+//     </>
+//   );
+// };
 
 export default App;
